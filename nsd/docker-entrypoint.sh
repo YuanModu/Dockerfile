57e0676bfd37df07fd37b2e387461cdb322475c2
Atila Satilmis <atila.satilmis@gmail.com>
2017-07-12 22:59:29 +0300

    blah

--- nsd/docker-entrypoint.sh
+++ nsd/docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ "$1" = 'nsd' ]; then
     # Clean files related to some previous run.
-    rm -f /run/nsd/nsd.pid
+    rm -f /var/run/nsd/nsd.pid
     rm -f /var/db/nsd/{nsd.db,xfrd.state}
 fi
 
