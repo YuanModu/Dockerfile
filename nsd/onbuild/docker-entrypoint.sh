8b1bb28c7d4fd008cce828bf7308e09c562d81dd
Atila Satilmis <atila.satilmis@gmail.com>
2017-07-14 11:30:08 +0300

    blah

--- nsd/onbuild/docker-entrypoint.sh
+++ nsd/onbuild/docker-entrypoint.sh
@@ -5,6 +5,7 @@ if [ "$1" = 'nsd' ]; then
     # Clean files related to some previous run.
     rm -f /var/run/nsd/nsd.pid
     rm -f /var/db/nsd/{nsd.db,xfrd.state}
+    install -d -o nsd -g nsd -m 0700 /var/run/nsd
 fi
 
 exec "$@"
