2e0e958d0d088f2c7526bf039831b175d81f2fd9
Atila Satilmis <atila.satilmis@gmail.com>
2017-07-23 23:51:47 +0300

    blah

--- nsd/onbuild/docker-entrypoint.sh
+++ nsd/onbuild/docker-entrypoint.sh
@@ -5,6 +5,8 @@ if [ "$1" = 'nsd' ]; then
     # Clean files related to some previous run.
     rm -f /var/run/nsd/nsd.pid
     rm -f /var/db/nsd/{nsd.db,xfrd.state}
+
+    # Since we have no init system, runtime folders have to be created manually.
     install -d -o nsd -g nsd -m 0700 /var/run/nsd
 fi
 
