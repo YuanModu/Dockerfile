ce8177683ee7b5bc740500208f9df8caf48bf1fc
Atila Satilmis <atila.satilmis@gmail.com>
2017-07-23 20:47:55 +0300

    vlah

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -1,27 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
-render() {
-    : > $2
-    while IFS= read line ; do
-        while [[ "$line" =~ (\$\{[a-zA-Z_][a-zA-Z_0-9]*\}) ]] ; do
-            LHS=${BASH_REMATCH[1]}
-            RHS="$(eval echo "\"$LHS\"")"
-            line=${line//$LHS/$RHS}
-        done
-        echo "$line" >> $2
-    done < $1
-}
-
 : ${NGINX_DOMAIN_NAME:=example.com}
-: ${NGINX_STATIC_ROOT:=${PROJECT_ROOT}/static}
 
 if [ "$1" = 'nginx' ]; then
-    if [ ! -h /etc/nginx/nginx.conf  ]; then
-        render ${PROJECT_ROOT}/nginx.conf.template ${PROJECT_ROOT}/nginx.conf
-        ln -sf ${PROJECT_ROOT}/nginx.conf /etc/nginx/nginx.conf
-    fi
-
+	sed "s|@@NGINX_DOMAIN_NAME@@|$NGINX_DOMAIN_NAME|" \
+		/nginx.conf.template > /etc/nginx/nginx.conf
 fi
 
 exec "$@"
