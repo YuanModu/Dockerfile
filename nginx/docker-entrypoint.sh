0bbf116f8457a3ec6b8392a8e4b032e57cec720a
Atila Satilmis <atila.satilmis@gmail.com>
2017-07-24 01:22:17 +0300

    blah

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -4,6 +4,9 @@ set -e
 : ${NGINX_DOMAIN_NAME:=example.com}
 
 if [ "$1" = 'nginx' ]; then
+        # Since we have no init system, runtime folders have to be created manually.
+        install -dm700 -o nginx -g nginx /run/nginx
+
 	sed "s|@@NGINX_DOMAIN_NAME@@|$NGINX_DOMAIN_NAME|" \
 		/nginx.conf.template > /etc/nginx/nginx.conf
 fi
