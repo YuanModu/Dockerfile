2bb42e8191d385663c052305d82c8db228947a19
Atila Satilmis <atila.satilmis@gmail.com>
2017-07-27 00:51:21 +0300

    blah

--- arm64v8/alpine/nginx/docker-entrypoint.sh
+++ arm64v8/alpine/nginx/docker-entrypoint.sh
@@ -2,6 +2,8 @@
 set -e
 
 : ${NGINX_DOMAIN_NAME:=example.com}
+: ${NGINX_DOMAIN_PORT:=80}
+: ${NGINX_UPSTREAM_SERVERS:=127.0.0.1:8000}
 
 if [ "$1" = 'nginx' ]; then
         # Clean files related to some previous run.
@@ -9,8 +11,10 @@ if [ "$1" = 'nginx' ]; then
 
         # Since we have no init system, runtime folders have to be created manually.
         install -dm700 -o nginx -g nginx /run/nginx
-
-	sed "s|@@NGINX_DOMAIN_NAME@@|$NGINX_DOMAIN_NAME|" \
+	NGINX_UPSTREAM_SERVERS=$(for SERVER in $NGINX_UPSTREAM_SERVERS; do printf "server %s\n" "$SERVER"; done)
+	sed -e "s|@@NGINX_DOMAIN_NAME@@|$NGINX_DOMAIN_NAME|" \
+	    -e "s|@@NGINX_DOMAIN_PORT@@|$NGINX_PORT|" \
+	    -e "s|@@NGINX_UPSTREAM_SERVERS@@|$NGINX_UPSTREAM_SERVERS|" \
 		/nginx.conf.template > /etc/nginx/nginx.conf
 fi
 
