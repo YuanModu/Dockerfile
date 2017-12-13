d68142fa1d8232cfff4ad1e7a1872fa2d335bb1c
Atila Satilmis <atila.satilmis@gmail.com>
2017-12-13 20:26:28 +0300

    blah

--- arm64v8/alpine/nginx/build/docker-entrypoint.sh
+++ arm64v8/alpine/nginx/build/docker-entrypoint.sh
@@ -13,9 +13,6 @@ if [ "$1" = 'nginx' ]; then
 
 	NGINX_UPSTREAM_SERVERS=$(for SERVER in $NGINX_UPSTREAM_SERVERS; do printf "server 
 %s:8000;\\\n\t\t" $SERVER; done)
-
-	# Remove the last newline and tabs.
-	NGINX_UPSTREAM_SERVERS=${NGINX_UPSTREAM_SERVERS::-4}
 	sed -e "s|@@NGINX_DOMAIN_NAME@@|$NGINX_DOMAIN_NAME|" \
 	    -e "s|@@NGINX_UPSTREAM_SERVERS@@|$NGINX_UPSTREAM_SERVERS|" \
 		/nginx.conf.template > /etc/nginx/nginx.conf
