d68c41ed0a81d63412ed36f8998c2e5564c9b610
Atila Satilmis <atila.satilmis@gmail.com>
2019-02-12 20:04:53 +0300

    blah

--- arm64v8/alpine/nginx/build/docker-entrypoint.sh
+++ arm64v8/alpine/nginx/build/docker-entrypoint.sh
@@ -5,13 +5,13 @@ set -e
 : ${NGINX_UPSTREAM_SERVERS:=127.0.0.1}
 
 if [ "$1" = 'nginx' ]; then
-	NGINX_UPSTREAM_SERVERS=$(for SERVER in $NGINX_UPSTREAM_SERVERS; do printf "server %s:8000;\\\n\t\t" $SERVER; done)
+  NGINX_UPSTREAM_SERVERS=$(for SERVER in $NGINX_UPSTREAM_SERVERS; do printf "server %s:8000;\\\n\t\t" $SERVER; done)
 
-	# Remove the last newline and tabs.
-	NGINX_UPSTREAM_SERVERS=${NGINX_UPSTREAM_SERVERS::-4}
-	sed -e "s|@@NGINX_DOMAIN_NAME@@|$NGINX_DOMAIN_NAME|" \
-	    -e "s|@@NGINX_UPSTREAM_SERVERS@@|$NGINX_UPSTREAM_SERVERS|" \
-		/nginx.conf.template > /etc/nginx/nginx.conf
+  # Remove the last newline and tabs.
+  NGINX_UPSTREAM_SERVERS=${NGINX_UPSTREAM_SERVERS::-4}
+  sed -e "s|@@NGINX_DOMAIN_NAME@@|$NGINX_DOMAIN_NAME|" \
+      -e "s|@@NGINX_UPSTREAM_SERVERS@@|$NGINX_UPSTREAM_SERVERS|" \
+      /etc/nginx/conf.d/python.conf.template > /etc/nginx/conf.d/python.conf
 fi
 
 exec "$@"
