17bac98d79087584c0398d86a43d168db7789ad7
Atila Satilmis <atila.satilmis@gmail.com>
2019-02-12 19:59:46 +0300

    blah

--- arm64v8/alpine/nginx/build/docker-entrypoint.sh
+++ arm64v8/alpine/nginx/build/docker-entrypoint.sh
@@ -5,12 +5,6 @@ set -e
 : ${NGINX_UPSTREAM_SERVERS:=127.0.0.1}
 
 if [ "$1" = 'nginx' ]; then
-  # Clean files related to some previous run.
-  rm -f /run/nginx/nginx.pid
-
-  # Since we have no init system, runtime folders have to be created manually.
-  install -dm700 -o nginx -g nginx /run/nginx
-
 	NGINX_UPSTREAM_SERVERS=$(for SERVER in $NGINX_UPSTREAM_SERVERS; do printf "server %s:8000;\\\n\t\t" $SERVER; done)
 
 	# Remove the last newline and tabs.
